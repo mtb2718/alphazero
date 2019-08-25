bee8e982e8e2b1710f931417b43c53de798bc723
Matt Bauch <mtb2718@gmail.com>
2019-08-24 22:28:28 -0700

    WIP MCTS

--- connectfour.py
+++ connectfour.py
@@ -1,4 +1,6 @@
 from itertools import product
+
+import torch
 import numpy as np
 from scipy.signal import convolve2d
 
@@ -7,86 +9,149 @@ GRID_WIDTH = 7
 GRID_HEIGHT = 6
 
 
+class AlphaC4(torch.nn.Module):
+
+    def __init__(self):
+        super(AlphaC4, self).__init__()
+        self._backbone = torch.nn.Sequential(
+            torch.nn.Conv2d(2, 16, (3, 3), padding=1, stride=1, bias=False),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(True),
+            torch.nn.Conv2d(16, 32, (3, 3), padding=1, stride=1, bias=False),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(True),
+            torch.nn.Conv2d(32, 32, (3, 3), padding=1, stride=1, bias=False),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(True),
+            torch.nn.Conv2d(32, 32, (3, 3), padding=1, stride=1, bias=False),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(True),
+            torch.nn.Conv2d(32, 8, (1, 1), padding=0, stride=1, bias=False),
+            torch.nn.BatchNorm2d(8),
+            torch.nn.ReLU(True),
+        )
+
+        self._policy_head = torch.nn.Sequential(
+            torch.nn.Conv2d(8, 6 * 7, (6, 7), padding=0, stride=1, bias=True),
+            torch.nn.ReLU(True),
+        )
+
+        self._value_head = torch.nn.Sequential(
+            torch.nn.Conv2d(8, 1, (6, 7), padding=0, stride=1, bias=True),
+        )
+
+    def forward(self, board):
+        feat = self._backbone(board)
+        p = self._policy_head(feat).reshape(-1, 1, 6, 7)
+        v = self._value_head(feat)
+        return p, v
+
+
 class ConnectFourState:
     def __init__(self):
-        self._board = np.zeros((2, GRID_HEIGHT, GRID_WIDTH), dtype=np.uint8)
-        self._turn = 0
+        self._history = []
 
-    @property
-    def board(self):
-        return self._board
 
     @property
     def turn(self):
-        return self._turn
+        return len(self._history) % 2
+
 
     @property
-    def valid_moves(self):
-        mask = np.zeros((GRID_HEIGHT, GRID_WIDTH), dtype=np.uint8)
-        cols = np.arange(GRID_WIDTH)
-        h = np.sum(self._board, axis=(0,1))
-        v = h < GRID_HEIGHT - 1
-        mask[h[v], cols[v]] = 1
-        return mask
+    def board(self):
+        # Render network input
+        board = np.zeros((2, GRID_HEIGHT, GRID_WIDTH), dtype=np.uint8)
+        num_filled = [0] * GRID_WIDTH
+        for i, col in enumerate(self._history):
+            player = i % 2
+            board[player, num_filled[col], col] = 1
+            num_filled[col] += 1
+        return board
+
+
+    @property
+    def valid_actions(self):
+        """Return list o valid actions player can take in this state.
+
+        A valid action in this case is represented by column index.
+        """
+        return [i for i in range(GRID_WIDTH) if self._history.count(i) < GRID_HEIGHT]
+
+
+    def eval(self, net):
+
+        # Channel 0 always equals current player's position
+        b = self.board
+        if self.turn == 1:
+            b[[0, 1]] = b[[1, 0]]
+        x = torch.from_numpy(b[np.newaxis, ...])
+        valid_mask = torch.zeros(GRID_HEIGHT, GRID_WIDTH, dtype=torch.float32)
+        for i in range(GRID_WIDTH):
+            valid_mask[self._history.count(i), i] = 1
+
+        p, v = net(x.float())
+        p *= valid_mask
+        if torch.sum(p) > 1e-5:
+            p /= torch.sum(p)
+        else:
+            p = valid_mask / torch.sum(valid_mask)
+
+        return p, v
+
 
     def copy(self):
         o = ConnectFourState()
-        o._board = self._board.copy()
-        o._turn = self._turn
+        o._history = self._history.copy()
         return o
 
-    def move(self, col):
+
+    def take_action(self, col):
         """Take current player's move.
         Args:
             col (int) - drop a piece in this column.
         Returns True on success, False for invalid moves.
         """
-        c = np.sum(self._board[:, :, col], axis=0)
-        occupied = np.nonzero(c)[0]
-        n = -1 if len(occupied) == 0 else occupied[-1]
-        if n < GRID_HEIGHT - 1:
-            self._board[self._turn, n + 1, col] = 1
-            self._turn = (self._turn + 1) % 2
+        if col in self.valid_actions:
+            self._history.append(col)
             return True
         return False
 
-    def terminal(self):
-        """Test whether this is a terminal game state.
+
+    @property
+    def winner(self):
+        """Test whether there is a winner in this game state.
 
         Returns:
-            done (bool) - True if game is over
-            draw (bool) - True if the game is a draw
-            winner (int) - 0/1, indicating winner if done=True, else -1
+            winner (int or None) - returns None if the game is not over, otherwise
+            returns the index of the winning player or -1 for a draw.
         """
-        done = False
-        draw = False
-        winner = -1
+        winner = None
 
         h0 = np.ones((1, 4)) / 4
         h1 = np.ones((4, 1)) / 4
         h2 = np.eye(4) / 4
         h3 = np.eye(4)[::-1] / 4
 
+        board = self.board
         for p, h in product([0, 1], [h0, h1, h2, h3]):
-            if np.any(convolve2d(self._board[p], h, 'valid') >= 1):
-                done = True
+            if np.any(convolve2d(board[p], h, 'valid') >= 1):
+                assert winner is None, 'Game cannot have multiple winners.'
                 winner = p
-                break
 
-        if not done and np.all(np.sum(self._board, axis=0) > 0):
-            done = True
-            draw = True
+        if winner is None and len(self.valid_actions) == 0:
+            return -1
+        return winner
 
-        return done, draw, winner
 
     def __str__(self):
         s = ''
+        board = self.board
         for r in reversed(range(GRID_HEIGHT)):
             rowstr = '|'
             for c in range(GRID_WIDTH):
-                if self._board[0, r, c] > 0:
+                if board[0, r, c] > 0:
                     rowstr += 'X'
-                elif self._board[1, r, c] > 0:
+                elif board[1, r, c] > 0:
                     rowstr += 'O'
                 else:
                     rowstr += ' '
@@ -100,7 +165,7 @@ if __name__ == '__main__':
     s = ConnectFourState()
     states = [s]
     print(s)
-    while True:
+    while s.winner is None:
         a = input(f'(Player {s.turn}) Take Action [0-{GRID_WIDTH - 1}, (u)ndo, (q)uit], (d)ebug: ')
         if a == 'q':
             exit(0)
@@ -112,7 +177,7 @@ if __name__ == '__main__':
             import pdb
             pdb.set_trace()
             continue
-        elif a in [str(i) for i in range(GRID_WIDTH)]:
+        elif int(a) in s.valid_actions:
             s = s.copy()
             if s.move(int(a)):
                 states.append(s)
@@ -122,11 +187,11 @@ if __name__ == '__main__':
         else:
             print('Invalid Selection.')
             continue
-        done, draw, winner = s.terminal()
         print(s)
-        if done:
-            if draw:
-                print('Tie!')
-            else:
-                print(f'Player {winner} wins!')
-            exit(0)
+        if s.winner is None:
+            continue
+        elif s.winner >= 0:
+            print(f'Player {s.winner} wins!')
+        else:
+            print('Tie!')
+        exit(0)
