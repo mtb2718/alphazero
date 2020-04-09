b7725514b6c0333c39b6390d451b0f86674242e7
Matt Bauch <mtb2718@gmail.com>
2020-04-09 15:44:52 -0700

    wip faster results

--- alphazero/tournament.py
+++ alphazero/tournament.py
@@ -1,8 +1,10 @@
 from argparse import ArgumentParser
 from itertools import product
+import multiprocessing as mp
 import os
 import pickle
 
+from tqdm import tqdm
 import torch
 
 from alphazero.config import AlphaZeroConfig
@@ -19,23 +21,40 @@ def load_ckpt(ckpt):
 
 
 def play_series(Game, player0, player1, N):
-    result = {}
+    outcome = {}
 
     # play one game greedily/deterministically
     game = Game()
     play(game, [player0, player1])
-    result['greedy'] = game.history
+    outcome['greedy'] = game.history
 
     # play N games with sampling
-    result['sample'] = []
+    outcome['sample'] = []
     player0.exploration = 1
     player1.exploration = 1
     for _ in range(N):
         game = Game()
         play(game, [player0, player1])
-        result['sample'].append(game.history)
+        outcome['sample'].append(game.history)
 
-    return result
+    return outcome
+
+
+def run_matchup(matchup):
+    c0, c1 = matchup
+    ckpt0 = os.path.join(c0[0], 'ckpt', c0[1])
+    m0, conf0 = load_ckpt(ckpt0)
+    m0 = m0.to(torch.device('cuda:0'))
+
+    ckpt1 = os.path.join(c1[0], 'ckpt', c1[1])
+    m1, conf1 = load_ckpt(ckpt1)
+    m1 = m1.to(torch.device('cuda:1'))
+
+    assert type(conf0.Game()) == type(conf1.Game()), 'Players must agree on game.'
+    p0 = AlphaZeroPlayer(m0, **c0[2])
+    p1 = AlphaZeroPlayer(m1, **c1[2])
+
+    return c0, c1, play_series(conf0.Game, p0, p1, 0)
 
 
 if __name__ == '__main__':
@@ -47,29 +66,20 @@ if __name__ == '__main__':
     logdir = '/data/alphazero/connect4/20200331-perf-sync-1gpu'
     ckpts = os.listdir(os.path.join(logdir, 'ckpt'))
     kwargs = {}
-    for ckpt in sorted(ckpts)[::2]:
+    for ckpt in sorted(ckpts)[::4]:
         candidate = (logdir, ckpt, kwargs)
         candidates.append(candidate)
 
     tournament_results = []
-    for c0, c1 in product(candidates, repeat=2):
-        ckpt0 = os.path.join(c0[0], 'ckpt', c0[1])
-        m0, conf0 = load_ckpt(ckpt0)
-        m0 = m0.cuda()
+    matchups = list(product(candidates, repeat=2))
 
-        ckpt1 = os.path.join(c1[0], 'ckpt', c1[1])
-        m1, conf1 = load_ckpt(ckpt1)
-        m1 = m1.cuda()
-
-        assert type(conf0.Game()) == type(conf1.Game()), 'Players must agree on game.'
-        p0 = AlphaZeroPlayer(m0, **c0[2])
-        p1 = AlphaZeroPlayer(m1, **c1[2])
-
-        tournament_results.append({
-            '0': c0,
-            '1': c1,
-            'outcome': play_series(conf0.Game, p0, p1, 16)
-        })
+    with mp.Pool(5) as pool:
+        for c0, c1, outcome in tqdm(pool.imap_unordered(run_matchup, matchups), total=len(matchups)):
+            tournament_results.append({
+                '0': c0,
+                '1': c1,
+                'outcome': outcome,
+            })
 
     with open('tournament_results.pkl', 'wb') as f:
         pickle.dump(tournament_results, f)
